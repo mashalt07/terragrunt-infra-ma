8648af2c8b42286e704e87998aac227637c0c92d
mashalt07 <maltamash2880@gmail.com>
2025-06-22 00:08:57 +0100

    making slight modifications to modules and github workflow

--- modules/organization-account/main.tf
+++ modules/organization-account/main.tf
@@ -1,3 +1,4 @@
+# Resource for creating a member account in an AWS organization.
 resource "aws_organizations_account" "this" {
   name  = var.name
   email = var.email 
--- modules/organization-account/variables.tf
+++ modules/organization-account/variables.tf
@@ -1,10 +1,13 @@
 variable "name" {
+    description = "Name of the account."
 }
 
 variable "email" {
+    description = "Email address of the owner to assign to the new member account"
     sensitive = true
 }
 
 variable "close_on_deletion" {
+    description = "Whether to close the account on a deletion event."
     default = true
 }
